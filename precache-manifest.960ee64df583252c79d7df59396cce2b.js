f011f374fc69aba6719a41de201ad6146b774c08
Cristian Zayas <chriss.zayas@gmail.com>
2020-05-15 08:04:07 -0300

    Fisrt my app

---
+++ precache-manifest.960ee64df583252c79d7df59396cce2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73abe65e704e7587c441b7c4084233a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "487a128ebcf112633afd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95bc1d91bf6b7784e565",
+    "url": "/static/js/2.e2404033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2404033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487a128ebcf112633afd",
+    "url": "/static/js/main.ea8c629b.chunk.js"
+  },
+  {
+    "revision": "1665c0961c5f6ce16ad4",
+    "url": "/static/js/runtime-main.e0809ed9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
